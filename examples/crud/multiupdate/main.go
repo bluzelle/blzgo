34df11f3ae38ad98e76d6e24548d6087f2aba78f
vbstreetz <vbstreetz@gmail.com>
2020-04-19 20:37:52 +0300

    chore: improve multiupdate example

--- examples/crud/multiupdate/main.go
+++ examples/crud/multiupdate/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/apex/log"
 	"github.com/vbstreetz/blzgo/examples/util"
 	"github.com/vbstreetz/blzgo"
+	"time"
+	"strconv"
 	// "os"
 )
 
@@ -21,9 +23,19 @@ func main() {
 		log.Fatalf("%s", err)
 	}
 
+	key1 := strconv.FormatInt(100 + time.Now().Unix(), 10)
+	key2 := strconv.FormatInt(200 + time.Now().Unix(), 10)
+
+    if err := ctx.Create(key1, "value", 0); err != nil {
+		log.Fatalf("%s", err)
+	}
+    if err := ctx.Create(key2, "value", 0); err != nil {
+        log.Fatalf("%s", err)
+    }
+
 	keyValues := []*bluzelle.KeyValue{}
-	keyValues = append(keyValues, &bluzelle.KeyValue{"foo", "bar"})
-	keyValues = append(keyValues, &bluzelle.KeyValue{"1", "2"})
+	keyValues = append(keyValues, &bluzelle.KeyValue{key1, "bar"})
+	keyValues = append(keyValues, &bluzelle.KeyValue{key2, "baz"})
 
 	log.Infof("updating keys(%s)...", keyValues)
 
